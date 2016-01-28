07210efe248010b728749737e379d35d82bfe9e3
acareaga <aaroncareaga@gmail.com>
2016-01-28 08:55:18 -0700

    Need to fix syntax error on store picker, for inventory

--- scripts/main.js
+++ scripts/main.js
@@ -2,72 +2,93 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
+var Router  = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var Navigation = ReactRouter.Navigation;
 var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
+/*
+  App
+*/
 
 var App = React.createClass({
+
   render : function() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market"/>
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order/>
         <Inventory/>
       </div>
-      )
+    )
   }
 });
 
+/*
+  Header
+  <Header/>
+*/
 var Header = React.createClass({
   render : function() {
     return (
       <header className="top">
         <h1>Catch
           <span className="ofThe">
-          <span className="of">of</span>
-          <span className="the">the</span>
+            <span className="of">of</span>
+            <span className="the">the</span>
           </span>
-        Day</h1>
-      <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+          Day</h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
     )
   }
-});
+})
 
+/*
+  Order
+  <Order/>
+*/
 var Order = React.createClass({
   render : function() {
     return (
       <p>Order</p>
     )
   }
-});
+})
 
+/*
+  Inventory
+  <Inventory/>
+*/
 var Inventory = React.createClass({
   render : function() {
     return (
       <p>Inventory</p>
     )
   }
-});
+})
+
+
+/*
+  StorePicker
+  This will let us make <StorePicker/>
+*/
 
 var StorePicker = React.createClass({
   mixins : [History],
   goToStore : function(event) {
     event.preventDefault();
+    // get the data from the input
     var storeId = this.refs.storeId.value;
     this.history.pushState(null, '/store/' + storeId);
   },
   render : function() {
-    var name = "Aaron";
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
-        <h2>Please Enter a Store</h2>
+        <h2>Please Enter A Store</h2>
         <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="Submit" />
       </form>
@@ -76,21 +97,27 @@ var StorePicker = React.createClass({
 
 });
 
+/*
+  Not Found
+*/
+
 var NotFound = React.createClass({
   render : function() {
-    return <h1>Not Found</h1>
+    return <h1>Not Found!</h1>
   }
-})
+});
 
 
-// ROUTES
+/*
+  Routes
+*/
 
 var routes = (
   <Router history={createBrowserHistory()}>
-    <Route path="/" component={StorePicker} />
-    <Route path="/store/:storeId" component={App} />
-    <Route path="*" component={NotFound} />
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+    <Route path="*" component={NotFound}/>
   </Router>
-  );
+)
 
 ReactDOM.render(routes, document.querySelector('#main'));
